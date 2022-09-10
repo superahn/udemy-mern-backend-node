6b4d0edd433f7710fc5d8dd2df71ad511bb7bc46
Sung Ahn <superahn@gmail.com>
2022-09-10 11:03:37 +1000

    Added login, signup and get users functions. Only simple password check using plain text

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -3,48 +3,6 @@ const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
 const Place = require("../models/place");
 
-let DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    creator: "u2",
-  },
-  {
-    id: "p3",
-    title: "Best Choice painting",
-    description: "Hire a Professional Painter!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "6 Towner Drive Knoxfield, VIC 3180",
-    location: {
-      lat: -37.88800152939171,
-      lng: 145.2439506426094,
-    },
-    creator: "u3",
-  },
-];
-
 const getPlacesById = async (req, res, next) => {
   const placeId = req.params.pid; // { pid: 'p1' }
 
--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,54 +1,95 @@
 const HttpError = require("../models/http-error");
 const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
+const User = require("../models/user");
 
-let DUMMY_USERS = [
-  {
-    id: "u1",
-    name: "Sung Ahn",
-    email: "test@test.com",
-    password: "testers",
-  },
-];
+const getUsers = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find({}, "-password");
+  } catch (err) {
+    const error = new HttpError(
+      "Fetching users failed, please try later.",
+      500
+    );
+    return next(error);
+  }
 
-const getUsers = (req, res, next) => {
-  res.json({ users: DUMMY_USERS });
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
 };
 
-const signup = (req, res, next) => {
-  // throw error:
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError("Invalid inputs passed, please check your data", 422);
+    return next(
+      new HttpError("Invalid inputs passed, please check your data", 422)
+    );
   }
 
-  const { name, email, password } = req.body;
+  const { name, email, password, places } = req.body;
 
   // check if there is a same email already.
-  const hasUser = DUMMY_USERS.find((u) => u.email == email);
-  if (hasUser) {
-    throw new HttpError("Could not create user, email already exist", 422);
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      "Signing up failed, please try again later.",
+      500
+    );
+    return next(error);
   }
 
-  const createdUser = {
-    id: uuidv4(),
-    name, // name: name
+  if (existingUser) {
+    const error = new HttpError(
+      "User exists already, please login instead.",
+      422
+    );
+    return next(error);
+  }
+
+  // create one user and save into DB
+  const createdUser = new User({
+    name,
     email,
     password,
-  };
+    image: "https://live.staticflickr.com/7631/26849088292_36fc52ee90_b.jpg",
+    places,
+  });
 
-  DUMMY_USERS.push(createdUser);
-  res.status(201).json({ user: createdUser });
+  try {
+    console.log(createdUser);
+    await createdUser.save(); // write data to mongodb
+  } catch (err) {
+    console.log(err);
+    // handle error, otherwise this server can be crashed...
+    const error = new HttpError("Creating user failed, please try again.", 500);
+    return next(error); // pass the error into an Express error handler with the next argument.
+  }
+
+  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
-const login = (req, res, next) => {
+
+const login = async (req, res, next) => {
   const { email, password } = req.body;
 
-  const identifiedfUser = DUMMY_USERS.find((u) => u.email === email);
-  if (!identifiedfUser || identifiedfUser.password !== password) {
-    throw new HttpError(
-      "Could not identify user, credentials seem to be wrong.",
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      "Logging in failed, please try again later.",
+      500
+    );
+    return next(error);
+  }
+
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError(
+      "Invalid credentials, could not log you in.",
       401
     );
+    return next(error);
   }
 
   res.status(200).json({ message: "Logged in!" });
