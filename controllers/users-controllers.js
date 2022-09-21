29425397667bd7f27b644a9497e75683e3978467
Sung Ahn <superahn@gmail.com>
2022-09-21 23:12:33 +1000

    return user.id when json.state(200)

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -89,7 +89,10 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  res.status(200).json({ message: "Logged in!" });
+  res.status(200).json({
+    message: "Logged in!",
+    user: existingUser.toObject({ getters: true }),
+  });
 };
 
 exports.getUsers = getUsers;
